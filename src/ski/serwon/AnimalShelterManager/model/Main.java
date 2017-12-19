d9fab9c7ceaa2d9ab0f0fe0576e78cac495dbd18
Paweł Serwoński <pawel.serwonski@gmail.com>
2017-12-20 00:25:20 +0100

    packages created

---
+++ src/ski/serwon/AnimalShelterManager/model/Main.java
@@ -0,0 +1,28 @@
+package ski.serwon.AnimalShelterManager.model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource(File.separator + "ski" +
+                File.separator + "serwon" + File.separator + "AnimalShelterManager" + File.separator
+                + "view" + File.separator + "MainWindow.fxml"));
+
+        primaryStage.setTitle("Animal Shelter Manager");
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
